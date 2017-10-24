77b9c4ba8cd38ac791a03cbdb8f8f0ed9600e228
Vedhas Deshpande <de.vedhas@gmail.com>
2017-10-24 00:14:29 -0400

    Initial type deduction

--- jsontoprotomessage.py
+++ jsontoprotomessage.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
+#########################
+# Convert JSON to PROTO 
+# definition
+#########################
+
 import json
-
-
-
 
 def main():
     with open("test.json") as in_file:
         json_obj = json.load(in_file)
         print json_obj
-
-
+        for key in json_obj:
+            print key + " is " + str(type(json_obj[key]))
 
 if __name__ == "__main__":
     main()
